50c587b7917506233253bc829b9c09926821a03c
Kudo Chien <kudo@expo.dev>
2023-05-23 00:58:03 +0800

    Add App.js example

--- src/App.js
+++ src/App.js
@@ -1,24 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import { View, Animated } from "react-native";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <View style={{ flex: 1 }}>
+      <Animated.View style={{ height: 64, transform: undefined }}>
+        <View style={{ width: 50, height: 50, backgroundColor: "green" }} />
+      </Animated.View>
+    </View>
   );
 }
 
